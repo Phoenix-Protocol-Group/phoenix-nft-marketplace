f6f5cba35de462218cb315f40f897ebf70e36c2d
Kaloyan Gangov <6922910+gangov@users.noreply.github.com>
2024-08-16 11:16:35 +0300

    adds a test where 2 users deploy 3 collections

--- contracts/deployer/src/tests.rs
+++ contracts/deployer/src/tests.rs
@@ -42,6 +42,57 @@ fn test_deploy_collection_from_contract() {
 }
 
 #[test]
+fn test_deploy_multiple_collections() {
+    let env = Env::default();
+    env.mock_all_auths();
+
+    let client =
+        CollectionsDeployerClient::new(&env, &env.register_contract(None, CollectionsDeployer));
+
+    // Upload the Wasm to be deployed from the deployer contract.
+    // This can also be called from within a contract if needed.
+    let wasm_hash = env.deployer().upload_contract_wasm(collections::WASM);
+
+    client.initialize(&wasm_hash);
+
+    let creator = Address::generate(&env);
+    let bob = Address::generate(&env);
+
+    let first_salt = BytesN::from_array(&env, &[0; 32]);
+    let first_collection_name = String::from_str(&env, "Stellar Kitties");
+
+    let second_salt = BytesN::from_array(&env, &[5; 32]);
+    let second_collection_name = String::from_str(&env, "Stellar Puppers");
+
+    let third_salt = BytesN::from_array(&env, &[10; 32]);
+    let third_collection_name = String::from_str(&env, "Horror of Cthulhu");
+
+    client.deploy_new_collection(&first_salt, &creator, &first_collection_name);
+    client.deploy_new_collection(&second_salt, &creator, &second_collection_name);
+    client.deploy_new_collection(&third_salt, &bob, &third_collection_name);
+
+    assert_eq!(
+        client.query_all_collections(),
+        vec![
+            &env,
+            first_collection_name.clone(),
+            second_collection_name.clone(),
+            third_collection_name.clone()
+        ]
+    );
+
+    assert_eq!(
+        client.query_collection_by_creator(&creator),
+        vec![&env, first_collection_name, second_collection_name]
+    );
+
+    assert_eq!(
+        client.query_collection_by_creator(&bob),
+        vec![&env, third_collection_name]
+    );
+}
+
+#[test]
 #[ignore = "once we rebase from main"]
 #[should_panic(
     expected = "Collections Deployer: Initialize: initializing the contract twice is not allowed"

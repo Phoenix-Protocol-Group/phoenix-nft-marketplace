b7c5d7b26be9b905980144c944c802f2cb6878af
Kaloyan Gangov <6922910+gangov@users.noreply.github.com>
2024-08-22 12:28:42 +0300

    tests

--- contracts/auctions/src/test.rs
+++ contracts/auctions/src/test.rs
@@ -1,1 +1,20 @@
+use soroban_sdk::{testutils::Address as _, Address, Env};
 
+use crate::{
+    contract::{MarketplaceContract, MarketplaceContractClient},
+    storage::ItemInfo,
+};
+
+#[test]
+fn should_create_auction() {
+    let env = Env::default();
+    env.mock_all_auths();
+
+    let seller = Address::generate(&env);
+    let item_info = ItemInfo {
+        item_address: Address::generate(&env),
+        item_id: 1u64,
+        minimum_price: Some(10),
+        buy_now_price: Some(50),
+    };
+}
